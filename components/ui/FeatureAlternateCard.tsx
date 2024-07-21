0ea72c674db011809d62e0880e26c1ad6c1ef44a
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-07-21 20:18:35 +0530

    project landing page

--- components/ui/FeatureAlternateCard.tsx
+++ components/ui/FeatureAlternateCard.tsx
@@ -31,54 +31,147 @@ const FeatureAlternateCard = React.forwardRef<
       className,
       ...props
     },
-    ref,
+    ref
   ) => {
     return (
-      <a
-        ref={ref}
-        {...props}
-        className={cn(
-          "grid grid-cols-1 items-center md:grid-cols-2",
-          className,
-        )}
-        href={link}
-      >
-        <div
-          className={cn(
-            parseInt(id) % 2 === 0 ? "md:order-2" : "md:order-1",
-            "mb-10 md:mb-0",
-          )}
-        >
-          <img
-            src={img}
-            alt="feature image"
-            className="h-full w-full rounded-md object-cover"
-          />
-        </div>
-
-        <div
-          className={cn(
-            "order-1 flex flex-col gap-5",
-            parseInt(id) % 2 === 0 ? "md:pr-14" : "md:pl-14",
-          )}
-        >
-          <h2 className="text-3xl font-semibold md:text-5xl">{name}</h2>
-          <p className="text-muted-foreground md:text-xl">{description}</p>
-
-          {subFeatures.map((sf, index) => {
-            return (
-              <div className="flex items-center gap-2" key={index}>
-                <div className="rounded-md border bg-primary-foreground p-2">
-                  {sf.icon}
+      <section>
+        <div className="px-8 py-24 mx-auto md:px-12 lg:px-32 max-w-7xl">
+          <div>
+            <h1 className="text-4xl font-semibold tracking-tighter text-gray-900 lg:text-6xl text-balance">
+              Building one pagers together,
+              <span className="text-gray-600">wherever and anywhere</span>
+            </h1>
+            <p className="mt-4 text-base font-medium text-gray-500 text-balance">
+              Control and added security. With decentralization, users have more
+              control over their data and transactions, and the platform is less
+              susceptible to malicious attacks.
+            </p>
+          </div>
+          <div className="space-y-2">
+            <div className="grid gap-2 mt-12 lg:grid-flow-col-dense lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-3">
+              <div className="max-w-lg min-w-full mx-auto">
+                <div className="flex h-full">
+                  <div className="flex flex-col justify-center p-8 border bg-gray-50 rounded-3xl max-w-none">
+                    <h2 className="font-medium text-gray-900">
+                      No additional restrictions
+                    </h2>
+                    <p className="mt-4 text-sm font-medium text-gray-500 text-pretty">
+                      You may not impose any additional legal terms or
+                      technological measures on the work that restrict the
+                      freedoms granted by the CC BY 3.0 License. This means you
+                      can't add extra conditions or restrictions beyond what the
+                      license allows.
+                    </p>
+                  </div>
                 </div>
-                <span className="md:text-xl">{sf.name}</span>
               </div>
-            );
-          })}
+              <div className="lg:col-span-2">
+                <div>
+                  <div className="relative h-full p-2 overflow-hidden border rounded-3xl">
+                    <img
+                      src="https://images.unsplash.com/photo-1518773553398-650c184e0bb3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29kZSUyMHJlc3RyaWN0aW9ufGVufDB8fDB8fHww"
+                      className="object-cover w-full border shadow-2xl rounded-2xl"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="grid gap-2 lg:grid-flow-col-dense lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-3">
+              <div className="max-w-lg min-w-full mx-auto lg:col-start-3">
+                <div className="flex h-full">
+                  <div className="flex flex-col justify-center p-8 border bg-gray-50 rounded-3xl max-w-none">
+                    <h2 className="font-medium text-gray-900">
+                      Rule of Attribution
+                    </h2>
+                    <p className="mt-4 text-sm font-medium text-gray-500 text-pretty">
+                      You must give appropriate credit to the original creator
+                      of the work. This typically includes providing the name of
+                      the author or licensor, a link to the original work, and
+                      indicating if changes were made.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="lg:col-start-1 lg:col-span-2">
+                <div>
+                  <div className="relative h-full p-2 overflow-hidden border rounded-3xl">
+                    <img
+                      src="https://images.unsplash.com/photo-1518773553398-650c184e0bb3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29kZSUyMHJlc3RyaWN0aW9ufGVufDB8fDB8fHww"
+                      className="object-cover w-full border shadow-2xl rounded-2xl"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="grid gap-2 lg:grid-flow-col-dense lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-3">
+              <div className="max-w-lg min-w-full mx-auto">
+                <div className="flex h-full">
+                  <div className="flex flex-col justify-center p-8 border bg-gray-50 rounded-3xl max-w-none">
+                    <h2 className="font-medium text-gray-900">
+                      Share alike (SA) absence
+                    </h2>
+                    <p className="mt-4 text-sm font-medium text-gray-500 text-pretty">
+                      The CC BY 3.0 License does not include a "Share Alike"
+                      (SA) provision. Unlike some other Creative Commons
+                      licenses, it does not require you to license any
+                      derivative works under the same terms. You can create
+                      adaptations or derivatives and license them differently if
+                      you choose.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="lg:col-span-2">
+                <div>
+                  <div className="relative h-full p-2 overflow-hidden border rounded-3xl">
+                    <img
+                      src="https://images.unsplash.com/photo-1518773553398-650c184e0bb3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29kZSUyMHJlc3RyaWN0aW9ufGVufDB8fDB8fHww"
+                      className="object-cover w-full border shadow-2xl rounded-2xl"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="grid gap-2 mt-2 text-center md:grid-cols-3">
+              <div>
+                <div className="mt-4">
+                  <p className="font-medium text-gray-900">
+                    No warranty disclaimer
+                  </p>
+                  <p className="mt-2 text-sm text-gray-500">
+                    The license comes with no warranties. The licensor provides
+                    the work "as is," and users must use it at their own risk.
+                  </p>
+                </div>
+              </div>
+              <div>
+                <div className="px-8 mt-4">
+                  <p className="font-medium text-gray-900">
+                    Modification Freedom
+                  </p>
+                  <p className="mt-2 text-sm text-gray-500">
+                    You can adapt, remix, transform, and build upon the licensed
+                    work.
+                  </p>
+                </div>
+              </div>
+              <div>
+                <div className="px-8 mt-4">
+                  <p className="font-medium text-gray-900">
+                    Commercial use allowed
+                  </p>
+                  <p className="mt-2 text-sm text-gray-500">
+                    You are allowed to use the licensed work for both
+                    non-commercial and commercial purposes.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </a>
+      </section>
     );
-  },
+  }
 );
 
 FeatureAlternateCard.displayName = "FeatureAlternateCard";
